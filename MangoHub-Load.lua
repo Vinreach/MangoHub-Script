b232745c813c2ed32ea8fd9bb7d3da510dce6be1
RoyRedRedVN <163697405+RoyRedRedVN@users.noreply.github.com>
2025-08-29 13:20:24 +0700

    Create MangoHub-Load.lua

---
+++ MangoHub-Load.lua
@@ -0,0 +1,7 @@
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vinreach/MangoHub/main/GameList.lua"))()
+
+local URL = Games[game.PlaceId]
+
+if URL then
+  loadstring(game:HttpGet(URL))()
+end
