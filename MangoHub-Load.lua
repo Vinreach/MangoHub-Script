0f21317fd5cfa001ac0f83fb1a72bbc3fbc232ef
RoyRedRedVN <163697405+RoyRedRedVN@users.noreply.github.com>
2025-08-29 13:38:22 +0700

    Update MangoHub-Load.lua

--- MangoHub-Load.lua
+++ MangoHub-Load.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vinreach/MangoHub/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vinreach/MangoHub-Script/refs/heads/main/MangoHub-ListGame.lua"))()
 
 local URL = Games[game.PlaceId]
 
